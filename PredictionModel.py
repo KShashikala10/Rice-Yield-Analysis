247863aef116f2519bb2abde8d16979474246dee
K Shashikala <167070344+KShashikala10@users.noreply.github.com>
2025-09-14 14:13:19 +0530

    PredictionModel.py

--- PredictionModel.py
+++ PredictionModel.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
 
-data = pd.read_csv("data/rice_yield_prediction.csv", delimiter=',')
+data = pd.read_csv("data/raw/rice_yield_prediction.csv", delimiter=',')
 data.rename(columns={
     'Temperature (Â°C)': 'Temperature (°C)',
     'Rainfall (mm/month)': 'Rainfall (mm/month)',
